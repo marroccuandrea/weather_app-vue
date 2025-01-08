5cdd8f704af8de5501514f5e350ed02d7ede6802
marroccuandrea <marroccuandrea@yahoo.it>
2025-01-08 17:55:07 +0100

    [add] Google Fonts and custom Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./src/**/*.{html,js,vue,jsx,tsx,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "weather-primary": "#00668A",
+        "weather-secondary": "#004E71",
+      },
+    },
+    fontFamily: {
+      Roboto: ["Roboto", "sans-serif"],
+    },
   },
   plugins: [],
 };
